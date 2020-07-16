d9e91d6627c82ee776b66203936f6e7017d03e03
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-07-16 23:56:48 +0300

    развязывает 4 задачи

---
+++ module-01/proba.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// let a = 10;
+// let b = 25;8
+// let c = a + b;
+// alert("Сума чисел" + c);
+// console.log(c);
+
+// let p = 3.14;
+// let r = 10;
+// console.log(Math.pow(r, 2) * p);
+
+// let name = "Саша";
+// let number = 30;
+// let subject = "путешествовать";
+// let result = `Здравствуйте, меня зовут ${name}, мне ${number} лет, мне нравится ${subject}`;
+// console.log(result);
+
---
+++ module-01/task-1.js
@@ -0,0 +1,64 @@
+// Task 1
+
+// const name = "Генератор защитного поля";
+// let price = 1000;
+// console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+// price = 2000;
+
+// console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+
+// Task 2
+// const total = 100;
+// const ordered = 50;
+// let ammount;
+
+// if (total > ordered) {
+//   ammount = "На складе недостаточно товаров!";
+// } else {
+//   ammount = "Заказ оформлен, с вами свяжется менеджер!";
+// }
+// console.log(ammount);
+
+// // Task 3
+// const ADMIN_PASSWORD = "jqueryismyjam";
+// let message = prompt("Введите пароль");
+// console.log(message);
+
+// if (message === null) {
+//   console.log("Отменено пользователем!");
+// } else if (message === ADMIN_PASSWORD) {
+//   console.log("Добро пожаловать!");
+// } else {
+//   console.log("Доступ запрещен, неверный пароль!");
+// }
+// alert(message);
+
+// // Task 4
+// let credits = 23580;
+// let pricePerDroid = 3000;
+// let totalPrice;
+
+// const droidCount = prompt("Колличество дроидов:");
+// console.log(droidCount);
+
+// if (droidCount === null) {
+//   console.log("Отменено пользователем!");
+// } else if (droidCount === String) {
+//   console.log(droidCount);
+// }
+
+// console.log((totalPrice = pricePerDroid * droidCount));
+
+// let ammount = credits - totalPrice;
+
+// if (totalPrice > credits) {
+//   console.log("Недостаточно средств на счету!");
+// } else {
+//   console.log(
+//     `Вы купили ${droidCount} дроидов, на счету осталось ${ammount} кредитов.`
+//   );
+// }
+
+// Task 5
+
+// Task 6
