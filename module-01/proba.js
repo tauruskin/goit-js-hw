d9b8d346d7f197e7c845024b97ffcb2be8ad9a26
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-07-17 17:05:27 +0300

    выполняет 6 задачу

--- module-01/proba.js
+++ module-01/proba.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 // let a = 10;
 // let b = 25;8
@@ -16,3 +16,23 @@
 // let result = `Здравствуйте, меня зовут ${name}, мне ${number} лет, мне нравится ${subject}`;
 // console.log(result);
 
+// Задача 1
+// const name = prompt("Какое официальное название JavaScript");
+
+// if (name === "ECMAScript") {
+//   console.log("Верно");
+// } else {
+//   console.log("Не знаете ? ECMAScript!");
+// }
+
+// // Задача 2
+// Записать в тернарном виде
+
+// if (a + b < 4) {
+//   result = "Мало";
+// } else {
+//   result = "Много";
+// }
+
+// Задача 3
+// Напишите условие if для проверки, что переменная age находится в диапазоне между 14 и 90 включительно.
---
+++ module-01/task.js
@@ -0,0 +1,110 @@
+"use strict";
+// Task 1
+
+const name = "Генератор защитного поля";
+let price = 1000;
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+price = 2000;
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов.`);
+
+// Task 2
+// const total = 100;
+// const ordered = 50;
+// let ammount;
+
+// if (total > ordered) {
+//   ammount = "На складе недостаточно товаров!";
+// } else {
+//   ammount = "Заказ оформлен, с вами свяжется менеджер!";
+// }
+// console.log(ammount);
+
+// // Task 3
+// const ADMIN_PASSWORD = "jqueryismyjam";
+// let message = prompt("Введите пароль");
+// console.log(message);
+
+// if (message === null) {
+//   console.log("Отменено пользователем!");
+// } else if (message === ADMIN_PASSWORD) {
+//   console.log("Добро пожаловать!");
+// } else {
+//   console.log("Доступ запрещен, неверный пароль!");
+// }
+// alert(message);
+
+// // Task 4
+// let credits = 23580;
+// let pricePerDroid = 3000;
+// let totalPrice;
+
+// const droidCount = prompt("Колличество дроидов:");
+// console.log(droidCount);
+
+// if (droidCount === null) {
+//   console.log("Отменено пользователем!");
+// } else if (droidCount === String) {
+//   console.log(droidCount);
+// }
+
+// console.log((totalPrice = pricePerDroid * droidCount));
+
+// let ammount = credits - totalPrice;
+
+// if (totalPrice > credits) {
+//   console.log("Недостаточно средств на счету!");
+// } else {
+//   console.log(
+//     `Вы купили ${droidCount} дроидов, на счету осталось ${ammount} кредитов.`);
+// }
+
+// Task 5
+
+// const country = prompt("Выберите страну доставки");
+// let price;
+// let userChoice;
+
+// if (country) {
+//   userChoice = country.toLowerCase();
+// }
+
+// switch (userChoice) {
+//   case "китай":
+//     price = 100;
+//     break;
+
+//   case "чили":
+//     price = 250;
+//     break;
+
+//   case "австралия":
+//     price = 170;
+//     break;
+
+//   case "индия":
+//     price = 80;
+//     break;
+
+//   case "ямайка":
+//     price = 120;
+//     break;
+
+//   default:
+//     alert("В вашей стране доставка не доступна");
+// }
+
+// console.log(`Доставка в ${userChoice} будет стоить ${price} кредитов`);
+
+// Task 6
+
+// let input;
+// let total = 0;
+
+// while (input !== null) {
+//   input = prompt("Введите число" + total);
+//   !isNaN(input)
+//     ? (total = total + +input)
+//     : alert("Было введено не число, попробуйте еще раз");
+// }
+// alert(`Общая сумма чисел равна ${total}`);
