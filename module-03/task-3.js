9b22f3eecfeba6a67f5d598783dee8ed7cf07363
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-07-22 22:34:18 +0300

    задание 4

--- module-03/task-3.js
+++ module-03/task-3.js
@@ -1,4 +1,9 @@
-// Задание 1
+// Задание 1 Напиши скрипт, который, для объекта user, последовательно:
+
+// добавляет поле mood со значением 'happy'
+// заменяет значение hobby на 'skydiving'
+// заменяет значение premium на false
+// выводит содержимое объекта user в формате ключ: значение используя Object.keys() и for...of
 
 // const user = {
 //   name: "Mango",
@@ -14,22 +19,104 @@
 //   console.log(`${key}: ${user[key]}`);
 // }
 
-// \\--------------------------------
+// \\------------------------------------------------------------
 // Задание 2
-// \\--------------------------------
+// \\------------------------------------------------------------
 
-// \\--------------------------------
+// const countProps = function (obj) {
+//   return Object.entries(obj).length;
+// };
+// // const countProps = (obj) => {
+// //   return Object.entries(obj).length;
+// // };
+
+// console.log(countProps({})); // 0
+
+// console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
+
+// \\------------------------------------------------------------
 // Задание 3
-// \\--------------------------------
+// \\------------------------------------------------------------
 
-// \\--------------------------------
+// const findBestEmployee = function (employees) {
+//   const employeeValues = Object.values(employees);
+//   let maxValue = employeeValues[1];
+//   for (let i = 0; i < employeeValues.length; i++) {
+//     if (maxValue < employeeValues[i]) {
+//       maxValue = employeeValues[i];
+//     }
+//     const bestEmployee = employeeValues.indexOf(maxValue);
+//     const employeeKeys = Object.keys(employees);
+//     return employeeKeys[bestEmployee];
+//   }
+// };
+
+// console.log(
+//   findBestEmployee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   })
+// ); // lorence
+
+// console.log(
+//   findBestEmployee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   })
+// ); // mango
+
+// console.log(
+//   findBestEmployee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   })
+// ); // lux
+
+// \\------------------------------------------------------------
 // Задание 4
-// \\--------------------------------
+// \\------------------------------------------------------------
 
-// \\--------------------------------
+// const countTotalSalary = function (employees) {
+//   const employeeSalary = Object.values(employees);
+//   let total = 0;
+//   for (const salary of employeeSalary) {
+//     total += salary;
+//   }
+//   return total;
+// };
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
+
+// \\------------------------------------------------------------
 // Задание 5
-// \\--------------------------------
+// \\------------------------------------------------------------
 
-// \\--------------------------------
+// \\------------------------------------------------------------
 // Задание 6
-// \\--------------------------------
+// \\------------------------------------------------------------
--- proba.js
+++ proba.js
@@ -144,3 +144,72 @@
 // const { name, stars, status } = hotel;
 
 // console.log(name, stars, status); // "Resort Hotel", 5, undefined
+
+// let schedule = {};
+// const isEmpty = function (obj) {};
+// isEmpty(schedule);
+
+// let schedule = {};
+
+// alert(isEmpty(schedule)); // true
+
+// schedule["8:30"] = "get up";
+
+// alert(isEmpty(schedule)); // false
+
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
+// return Object
+// console.log(isEmpty(schedule));
+
+// // \\
+// \\
+// \\
+// \\
+// \\
+// \\
+// \\
+// \\
+
+// console.log();
+// isEmpty(schedule); // true
+
+// schedule["8:30"] = "get up";
+
+// console.log();
+// isEmpty(schedule); // false
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+// let sum = 0;
+// for (let key in salaries) {
+//   sum = sum + salaries[key];
+// }
+// console.log(sum);
+
+//  #4
+// Создайте функцию multiplyNumeric(obj), которая умножает
+// все числовые свойства объекта obj на 2.
+
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+// };
+// const multiplyNumeric = function (obj) {
+//   // let arr = Object.values(obj);
+//   for (let key in obj) {
+//     if (typeof obj[key] === "number") {
+//       obj[key] *= 2;
+//     }
+//   }
+// };
+// multiplyNumeric(menu);
+// console.log(menu);
