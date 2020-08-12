c4a5b28833f72630599fc79a7440ca1af3426eed
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-08-12 15:01:17 +0300

    задание 3

--- module-11/task1/task1.js
+++ module-11/task1/task1.js
@@ -1,37 +1,37 @@
-// const colors = [
-//   "#FFFFFF",
-//   "#2196F3",
-//   "#4CAF50",
-//   "#FF9800",
-//   "#009688",
-//   "#795548",
-// ];
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
 
-// const refs = {
-//   start: document.querySelector(`[data-action="start"]`),
-//   stop: document.querySelector(`[data-action="stop"]`),
-//   body: document.querySelector(`body`),
-// };
-// console.log(refs);
+const refs = {
+  start: document.querySelector(`[data-action="start"]`),
+  stop: document.querySelector(`[data-action="stop"]`),
+  body: document.querySelector(`body`),
+};
+console.log(refs);
 
-// let changeColorsInterval;
-// refs.start.addEventListener("click", flashColors);
-// refs.stop.addEventListener("click", stopFlashingColors);
+let changeColorsInterval;
+refs.start.addEventListener("click", flashColors);
+refs.stop.addEventListener("click", stopFlashingColors);
 
-// function flashColors() {
-//   refs.start.removeEventListener("click", flashColors);
-//   changeColorsInterval = setInterval(changeBodyColor, 1000);
-// }
+function flashColors() {
+  refs.start.removeEventListener("click", flashColors);
+  changeColorsInterval = setInterval(changeBodyColor, 1000);
+}
 
-// function changeBodyColor() {
-//   refs.body.style.backgroundColor =
-//     colors[randomIntegerFromInterval(0, colors.length - 1)];
-// }
-// function stopFlashingColors() {
-//   clearInterval(changeColorsInterval);
-//   refs.start.addEventListener("click", flashColors);
-// }
+function changeBodyColor() {
+  refs.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+function stopFlashingColors() {
+  clearInterval(changeColorsInterval);
+  refs.start.addEventListener("click", flashColors);
+}
 
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
--- module-11/task3/task3.js
+++ module-11/task3/task3.js
@@ -1,29 +1,64 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.id = document.querySelector(selector);
+    this.refs = {
+      days: this.id.querySelector(`[data-value="days"]`),
+      hours: this.id.querySelector(`[data-value="hours"]`),
+      mins: this.id.querySelector(`[data-value="mins"]`),
+      secs: this.id.querySelector(`[data-value="secs"]`),
+    };
+    this.time = targetDate.getTime();
+    setInterval(this.counter.bind(this), 1000);
+  }
+
+  //  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+  //  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+
+  counter() {
+    if (this.time - Date.now() > 0) {
+      this.refs.days.textContent = Math.floor(
+        (this.time - Date.now()) / (1000 * 60 * 60 * 24)
+      )
+
+        //  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+        //  * остатка % и делим его на количество миллисекунд в одном часе
+        //  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+
+        .toString()
+        .padStart(2, "0");
+      this.refs.hours.textContent = Math.floor(
+        ((this.time - Date.now()) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      )
+
+        //  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+        //  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+
+        .toString()
+        .padStart(2, "0");
+      this.refs.mins.textContent = Math.floor(
+        ((this.time - Date.now()) % (1000 * 60 * 60)) / (1000 * 60)
+      )
+
+        //  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+        //  * миллисекунд в одной секунде (1000)
+
+        .toString()
+        .padStart(2, "0");
+      this.refs.secs.textContent = Math.floor(
+        ((this.time - Date.now()) % (1000 * 60)) / 1000
+      )
+
+        .toString()
+        .padStart(2, "0");
+    } else {
+      this.refs.days.textContent = "00";
+      this.refs.hours.textContent = "00";
+      this.refs.mins.textContent = "00";
+      this.refs.secs.textContent = "00";
+    }
+  }
+}
 new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
+  targetDate: new Date("Aug 17, 2020"),
 });
-
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
