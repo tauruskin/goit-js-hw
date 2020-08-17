b8ffee86cc2628eb1c09f980d7ae24ade444a79d
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-08-17 16:06:09 +0300

    убирает лишний код

--- module-11/task3/task3.js
+++ module-11/task3/task3.js
@@ -19,6 +19,7 @@ function updateTimer(time) {
   );
   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
   refs.daysRef.textContent = `${days}`;
   refs.hoursRef.textContent = `${hours}`;
   refs.minsRef.textContent = `${mins}`;
@@ -34,11 +35,3 @@ const refs = {
   minsRef: document.querySelector('span[data-value="mins"]'),
   secsRef: document.querySelector('span[data-value="secs"]'),
 };
-
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-const secs = Math.floor((time % (1000 * 60)) / 1000);
